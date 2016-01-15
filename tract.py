60743143164611f98c732d28af752aa25a52e8ab
Morgan Peterson <morgan@gophronesis.com>
2016-01-15 16:36:30 -0500

    small optimizations

--- tract.py
+++ tract.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # tract.py
 
-from osgeo import ogr, osr
+from osgeo import ogr, osr, gdal
 
 import ijson
 import json
@@ -21,7 +21,6 @@ else:
     out_file   = '{0}.tract.json'.format(head_obj)
     shape_file = '{0}'.format(shape_obj)
 
-
 field_nmes     = []
 g_field_count  = 12
 lat_name       = 'latitude'
@@ -29,34 +28,31 @@ lon_name       = 'longitude'
 out_json_obj   = 'census_tract'
 
 
-def fmt_return_json(field_names, field_values, fc):
+def fmt_return_json(field_values, fc):
     """ Format all the shapefile values into a final JSON object that will
     attached to the object that came in. """
     x   = ','
-    seq = []
-    for fc in range(fc):
+
+    def builder(ele):
         if field_values is None:
             fv = 'null'
         else:
-            fv = field_values.GetFieldAsString(fc)
+            fv = field_values.GetFieldAsString(ele)
 
-        seq.append('"{0}":"{1}"'.format(\
-                field_nmes[fc],fv))
+        x = '"{0}":"{1}"'.format(field_nmes[ele],fv)
+        return x
 
-    json_comp = "{"+x.join(seq)+"}"
-    return json.loads(json_comp)
+    seq = [builder(fc) for fc in xrange(fc)]
+    return json.loads("{{{0}}}\n".format(x.join(seq)))
 
 
-def get_census_tract(polylayer, longitude, latitude):
+def get_census_tract(polylayer, pref, longitude, latitude):
     """ takes in all layers and searches through them looking for the tract
     that matches our longitude and latitude values. If a tract is not found,
     then it returns an object with null fields"""
     try:
-        pnt_ref       = osr.SpatialReference()
-        pnt_ref.ImportFromEPSG(4326)
-
         geo_ref       = polylayer.GetSpatialRef()
-        ctran         = osr.CoordinateTransformation(pnt_ref, geo_ref)
+        ctran         = osr.CoordinateTransformation(pref, geo_ref)
         [lon, lat, z] = ctran.TransformPoint(longitude, latitude)
         point         = ogr.Geometry(ogr.wkbPoint)
         point.SetPoint_2D(0, lon, lat)
@@ -68,12 +64,12 @@ def get_census_tract(polylayer, longitude, latitude):
     if p is None:
         # if p is None then there was a fault in the longitude and/or
         # latitude values and we eturn an object with field values of null
-        return fmt_return_json(field_nmes, None, g_field_count)
+        return fmt_return_json(None, g_field_count)
 
     if point.Within(p.GetGeometryRef()):
         # if we do have longitude and latitude then we loop through the
         # layers fields and make and object that we then return
-        return fmt_return_json(field_nmes, p, g_field_count)
+        return fmt_return_json(p, g_field_count)
 
 
 def get_parent_field(our_head_object):
@@ -90,13 +86,15 @@ if sf is None:
     print "Could not open {0}".format(shape_file)
     sys.exit(1)
 
-
+# set all osgeo.gdal options
 layers        = sf.GetLayer()
 polydef       = layers.GetLayerDefn()
 g_field_count = polydef.GetFieldCount()
+pnt_ref       = osr.SpatialReference()
+pnt_ref.ImportFromEPSG(4326)
 
-for i in range(g_field_count):
-    field_nmes.append(polydef.GetFieldDefn(i).GetName()[:-2])
+field_nmes = \
+        [polydef.GetFieldDefn(x).GetName()[:-2] for x in xrange(g_field_count)]
 
 parent_field = get_parent_field(head_obj)
 
@@ -123,15 +121,14 @@ with open(in_file,'rb') as inf:
             our_lon = ele[lon_name]
             our_lat = ele[lat_name]
         # append our census tract object to our JSON object
-        ele[out_json_obj] = get_census_tract(layers, our_lon, our_lat)
+        ele[out_json_obj] = get_census_tract(layers, pnt_ref, our_lon, our_lat)
         json.dump(ele, outf)
-        outf.write('\n')
 
         # all for the console progress tracker
         out_size = float(os.stat(out_file).st_size)
         sys.stdout.write(\
-                '\rIn: {0}B - Out: {1}B - {2}%'.format(\
-                in_size, out_size, round(out_size/in_size*100)))
+                '\rIn: {0}MB - Out: {1:.0f}MB - {2:.2f}%'.format(\
+                (in_size/1000000), (out_size/1000000), out_size/in_size*100))
         sys.stdout.flush()
 
 # erase all console output
@@ -140,4 +137,5 @@ sys.stdout.flush()
 
 #close write file
 outf.close()
+
 
